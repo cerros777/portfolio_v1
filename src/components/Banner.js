aea1ade231a7617f8aec8db346ac8ab131dc52dd
dc7797 <110956445+cerros777@users.noreply.github.com>
2024-06-07 16:40:07 -0600

    new title

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ import TrackVisibility from 'react-on-screen';
 export const Banner = () => {
     const [loopNum, setLoopNum] = useState (0);
     const [isDeleting, setIsDeleting] = useState (false);
-    const toRotate = ["Web Developer", "Bubble Developer", "Fullstack Developer"];
+    const toRotate = ["Web", "Bubble", "Fullstack"];
     const [text, setText] = useState('');
     const [delta, setDelta] = useState(100 - Math.random() * 100);
     const period = 100;
@@ -52,7 +52,7 @@ export const Banner = () => {
                     {({isVisible}) =>
                     <div className={isVisible ? "animate__animated animate__bounce" : ""}>
                     <span className="tagline">Welcome</span>
-                    <h1>{`Hi I'm Daniel, `}<span className="wrap">{text}</span></h1>
+                    <h1>{`Hi I'm Daniel, `}<span className="wrap">{text}</span>Developer</h1>
                     <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                     <button onClick={() => console.log('connect')}>Let's connect <ArrowRightCircle size={25}></ArrowRightCircle></button>                
                     </div>}
