c90dcca7efe778e9834832605cc3ad0d8355f851
dc7797 <110956445+cerros777@users.noreply.github.com>
2024-06-07 15:02:54 -0600

    portfolio

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require("express");
+const path = require("path");
+const router = express.Router();
+const cors = require("cors");
+require('dotenv').config();
+const nodemailer = require("nodemailer");
+
+// server used to send emails
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.firstName + " " + req.body.lastName;
+  const email = req.body.email;
+  const message = req.body.message;
+  const phone = req.body.phone;
+  const mail = {
+    from: name,
+    to: "cerros384@gmail.com",
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Phone: ${phone}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
+
+// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server Running on port ${port}`);
+});
