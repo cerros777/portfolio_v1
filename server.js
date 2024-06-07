979c2045de78d94afdabdf9ed63d9fc54298255d
dc7797 <110956445+cerros777@users.noreply.github.com>
2024-06-07 15:06:49 -0600

    email hidden

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ router.post("/contact", (req, res) => {
   const phone = req.body.phone;
   const mail = {
     from: name,
-    to: "cerros384@gmail.com",
+    to: process.env.EMAIL_USER,
     subject: "Contact Form Submission - Portfolio",
     html: `<p>Name: ${name}</p>
            <p>Email: ${email}</p>
